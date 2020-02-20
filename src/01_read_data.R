1ec69175fa2af46a885cad96cb07bc8d7827c682
Karen White <audreywhite@Audreys-MacBook-Air.local>
2020-02-20 11:49:48 -0500

    Added silly hipster text to read_data file as an example

---
+++ src/01_read_data.R
@@ -0,0 +1,6 @@
+From hipsum.co
+Fixie brooklyn raclette next level, pickled la croix flexitarian succulents kinfolk tacos retro artisan blog stumptown bitters. Gluten-free kogi lyft bushwick vape, chicharrones leggings yr street art kitsch migas literally hammock cloud bread. Tilde pinterest pickled tousled affogato. Vaporware portland poke, tilde banh mi blog forage affogato. Organic church-key bespoke chia iPhone taxidermy heirloom artisan williamsburg pitchfork.
+
+Oh. You need a little dummy text for your mockup? How quaint.
+
+I bet you’re still using Bootstrap too…
