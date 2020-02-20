71a6ad0d4d3075d4dcf8c6bada55cdbb4b147e71
Audrey White <audrey.white@duke.edu>
2020-02-20 14:33:20 -0500

    Copying code from DUMLpreparingData.R

--- src/01_read_data.R
+++ src/01_read_data.R
@@ -1,6 +1,21 @@
-From hipsum.co
-Fixie brooklyn raclette next level, pickled la croix flexitarian succulents kinfolk tacos retro artisan blog stumptown bitters. Gluten-free kogi lyft bushwick vape, chicharrones leggings yr street art kitsch migas literally hammock cloud bread. Tilde pinterest pickled tousled affogato. Vaporware portland poke, tilde banh mi blog forage affogato. Organic church-key bespoke chia iPhone taxidermy heirloom artisan williamsburg pitchfork.
+library(dplyr)
+library(readr)
+library(fields)
+library(lme4)
+library(ggplot2)
+library(here)
 
-Oh. You need a little dummy text for your mockup? How quaint.
+# Read in data
+# positional data about the RV Kahuna
+# kahuna <- read_csv('../../../presentations/2018/2018-12-03_DUML-RepResearch/data/2018-11-26_2017-Cape-Hatteras-BRS-kahuna-CEE.csv') 
+kahuna <- read_csv(here::here('data', '2018-11-26_2017-Cape-Hatteras-BRS-kahuna-CEE.csv'))
+kStart <- kahuna %>% 
+  filter(status == 'start')
 
-I bet you’re still using Bootstrap too…
+# Read in Gm182 Data: 100 estimated positions of Gm182, augmented with focal follow data
+gm182UP <- read_csv(here::here('data', '2018-11-27_Gm182-UserPoints-Start-CEE-Locations-Kahuna.csv')) %>% 
+  mutate(status = 'userPoints')
+
+# Read in Gm182 Data: 100 estimated positions of Gm182
+gm182 <- read_csv(here::here('data', '2018-11-27_Gm182-Start-CEE-Locations-Kahuna.csv')) %>% 
+  mutate(status = 'noUserPoints')
